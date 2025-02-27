99eb38af1a69116b17f96c382e648dbfb986bbcd
Connor Mayer <cjmayer@uci.edu>
2025-02-26 20:11:05 -0800

    Added a few necessary downloads

--- nltk_word_counts.py
+++ nltk_word_counts.py
@@ -2,11 +2,15 @@ import nltk
 from nltk.corpus import stopwords
 from string import punctuation
 
+# This will download some data you need for word tokenization
+# and stopword removal. This might take a few moments the first
+# time you run it.
+nltk.download('punkt')
+nltk.download('punkt_tab') 
+nltk.download('stopwords')
+
 # This contains the list of words/punctuation we want to remove
 STOP_WORDS = list(punctuation) + ["--", "''", "``"] + stopwords.words('english')
-
-# This will download some data you need for word tokenization
-nltk.download('punkt')
 
 # Your implementation of get_most_frequent_words goes here
 
